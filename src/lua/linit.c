a62a62be2369b8efd29ba9910ca29924f42498c8
migueletto <migueletto@yahoo.com>
2023-03-01 17:24:30 -0300

    Removed unused modules from Lua.

--- src/lua/linit.c
+++ src/lua/linit.c
@@ -41,17 +41,17 @@
 */
 static const luaL_Reg loadedlibs[] = {
   {"_G", luaopen_base},
-  {LUA_LOADLIBNAME, luaopen_package},
-  {LUA_COLIBNAME, luaopen_coroutine},
+  //{LUA_LOADLIBNAME, luaopen_package},
+  //{LUA_COLIBNAME, luaopen_coroutine},
   {LUA_TABLIBNAME, luaopen_table},
   {LUA_IOLIBNAME, luaopen_io},
-  {LUA_OSLIBNAME, luaopen_os},
+  //{LUA_OSLIBNAME, luaopen_os},
   {LUA_STRLIBNAME, luaopen_string},
-  {LUA_MATHLIBNAME, luaopen_math},
-  {LUA_UTF8LIBNAME, luaopen_utf8},
-  {LUA_DBLIBNAME, luaopen_debug},
+  //{LUA_MATHLIBNAME, luaopen_math},
+  //{LUA_UTF8LIBNAME, luaopen_utf8},
+  //{LUA_DBLIBNAME, luaopen_debug},
 #if defined(LUA_COMPAT_BITLIB)
-  {LUA_BITLIBNAME, luaopen_bit32},
+  //{LUA_BITLIBNAME, luaopen_bit32},
 #endif
   {NULL, NULL}
 };
