483250fef1c151244dd5892372f8768b51e485a2
migueletto <migueletto@yahoo.com>
2023-03-29 19:58:35 -0300

    Testing dialogs with zig.

--- src/zigtest/zigpumpkin.h
+++ src/zigtest/zigpumpkin.h
@@ -1,2 +1,3 @@
 void debug_full(const char *file, const char *func, int line, int level, const char *sys, const char *fmt, ...);
 void pumpkin_puts(const char *s);
+unsigned short FrmCustomAlert(unsigned short alertId, const char *s1, const char *s2, const char *s3);
