afaecde447cebeed39f81081626b061b2e03e146
migueletto <migueletto@yahoo.com>
2023-03-28 21:39:18 -0300

    Using @cDefine instead of a constant.

--- src/zigtest/src/main.zig
+++ src/zigtest/src/main.zig
@@ -2,9 +2,8 @@ const std = @import("std");
 
 const pumpkin = @cImport({
     @cInclude("zigpumpkin.h");
+    @cDefine("DEBUG_INFO", "1");
 });
-
-const DEBUG_INFO = 1;
 
 export fn CommandMain(c_argc: c_int, c_argv: [*c][*c]u8) c_int {
     var argc = c_argc;
@@ -12,7 +11,7 @@ export fn CommandMain(c_argc: c_int, c_argv: [*c][*c]u8) c_int {
 
     var i: u32 = 0;
     while (i < argc) {
-        pumpkin.debug_full("", "", 0, DEBUG_INFO, "zig", "arg %d: %s", i, argv[i]);
+        pumpkin.debug_full("", "", 0, pumpkin.DEBUG_INFO, "zig", "arg %d: %s", i, argv[i]);
         i = i + 1;
     }
 
