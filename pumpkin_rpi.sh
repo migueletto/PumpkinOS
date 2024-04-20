004f00db877a21eda6dc1356c61e713f91129351
Andrew Udvare <audvare@gmail.com>
2024-04-19 21:25:29 -0400

    pumpkin_rpi: lint and allow to run from any path

--- pumpkin_rpi.sh
+++ pumpkin_rpi.sh
@@ -1,15 +1,17 @@
+#!/usr/bin/env bash
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PATH=./bin:$PATH
 export LD_LIBRARY_PATH=./bin
 
 add_gpio() {
-  if [ ! -L /sys/class/gpio/gpio$1 ]; then
-    echo $1 > /sys/class/gpio/export
+  if [ ! -L "/sys/class/gpio/gpio$1" ]; then
+    echo "$1" > /sys/class/gpio/export
   fi
 }
 
 del_gpio() {
-  if [ -L /sys/class/gpio/gpio$1 ]; then
-    echo $1 > /sys/class/gpio/unexport
+  if [ -L "/sys/class/gpio/gpio$1" ]; then
+    echo "$1" > /sys/class/gpio/unexport
   fi
 }
 
@@ -18,7 +20,7 @@ add_gpio 25
 echo -e '\033[?17;0;0c' > /dev/tty1
 
 #./pumpkin -d 1 -f pumpkin.log -s libscriptlua.so ./script/pumpkin_linux.lua
-./pumpkin -d 1 -s libscriptlua.so ./script/pumpkin_linux.lua
+"${script_dir}/pumpkin" -d 1 -s libscriptlua.so ./script/pumpkin_linux.lua
 
 del_gpio 24
 del_gpio 25
