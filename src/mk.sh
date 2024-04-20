36fc3395bf240377e2c3d3c5439e5577aa4d8fa6
Andrew Udvare <audvare@gmail.com>
2024-04-19 21:26:09 -0400

    mk: lint

--- src/mk.sh
+++ src/mk.sh
@@ -12,18 +12,18 @@ OSNAME=$1
 BITS=$2
 TARGET=$3
 
-DIR=`pwd`
+DIR=$(pwd)
 ROOT=$DIR/..
 PATH=$ROOT/bin:$PATH
-export LD_LIBRARY_PATH=$ROOT/bin
+export LD_LIBRARY_PATH="$ROOT/bin"
 
-if [ $OSNAME = "Msys" ]; then
+if [ "$OSNAME" = "Msys" ]; then
   SDL2=
   GUI=windows
-elif [ $OSNAME = "GNU/Linux" ]; then
+elif [ "$OSNAME" = "GNU/Linux" ]; then
   SDL2=liblsdl2
   GUI=linux
-elif [ $OSNAME = "Serenity" ]; then
+elif [ "$OSNAME" = "Serenity" ]; then
   if [ -z "$SERENITY" ]; then
     echo "Environment variable SERENITY must point to SerenityOS home directory"
     exit 1
@@ -37,27 +37,27 @@ fi
 
 for dir in bin lib tools vfs/app_card/PALM/Programs vfs/app_install vfs/app_storage registry
 do
-  if [ ! -d $ROOT/$dir ]; then
+  if [ ! -d "$ROOT"/$dir ]; then
     echo "creating directory $dir"
-    mkdir -p $ROOT/$dir
+    mkdir -p "$ROOT"/$dir
   fi
 done
 
 for dir in pilrc prcbuild
 do
   if [ -d $dir ]; then
-    cd $dir
-    make ROOT=$ROOT BITS=$BITS $TARGET
-    cd $DIR
+    cd $dir || exit
+    make ROOT="$ROOT" BITS="$BITS" "$TARGET"
+    cd "$DIR" || exit
   fi
 done
 
 for dir in libpit lua $SDL2 libpumpkin libos libshell $GUI BOOT Launcher Preferences Command Edit LuaSyntax MemoPad AddressBook ToDoList DateBook
 do
-  if [ -d $dir ]; then
-    cd $dir
-    make ROOT=$ROOT OSNAME=$OSNAME BITS=$BITS SERENITY=$SERENITY $TARGET
-    cd $DIR
+  if [ -d "$dir" ]; then
+    cd "$dir" || exit
+    make ROOT="$ROOT" OSNAME="$OSNAME" BITS="$BITS" SERENITY="$SERENITY" "$TARGET"
+    cd "$DIR" || exit
   fi
 done
 
