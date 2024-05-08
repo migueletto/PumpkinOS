88794cb238c80870b9ee9211b8d83a47b23ceb20
migueletto <migueletto@yahoo.com>
2024-05-08 17:47:00 -0300

    Revert to original form

--- pumpkin.sh
+++ pumpkin.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PATH=./bin:$PATH
 export LD_LIBRARY_PATH=./bin
-"${script_dir}/pumpkin" -d 1 -f pumpkin.log -s libscriptlua.so ./script/pumpkin_linux.lua
+./pumpkin -d 1 -f pumpkin.log -s libscriptlua.so ./script/pumpkin_linux.lua
