722592a77f100700fe8cbe97f51bfca5c9e9ef17
migueletto <migueletto@yahoo.com>
2024-10-13 09:00:16 -0300

    Add missing zipconf.h header in libzip.

---
+++ src/libzip/zipconf.h
@@ -0,0 +1,25 @@
+#include "sys.h"
+
+#define _Nonnull
+#define _Nullable
+
+#define ZIP_INT16_MAX  INT16_MAX
+#define ZIP_UINT16_MAX UINT16_MAX
+
+#define ZIP_INT32_MAX  INT32_MAX
+#define ZIP_UINT32_MAX UINT32_MAX
+
+#define ZIP_INT64_MAX  INT64_MAX
+#define ZIP_UINT64_MAX UINT64_MAX
+
+#define LIBZIP_VERSION "PumpkinOS libzip"
+
+typedef uint8_t zip_uint8_t;
+typedef uint16_t zip_uint16_t;
+typedef uint32_t zip_uint32_t;
+typedef uint64_t zip_uint64_t;
+
+typedef int8_t zip_int8_t;
+typedef int16_t zip_int16_t;
+typedef int32_t zip_int32_t;
+typedef int64_t zip_int64_t;
