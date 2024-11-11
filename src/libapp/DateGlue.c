84b23e1d81ed5616c9dd72eff11835c94ae5d9f3
migueletto <migueletto@yahoo.com>
2024-11-11 19:21:54 -0300

    Removed libapp.

--- src/libapp/DateGlue.c
+++
@@ -1,9 +0,0 @@
-#include <PalmOS.h>
-
-UInt16 DateGlueTemplateToAscii(const Char *templateP, UInt8 months, UInt8 days, UInt16 years, Char *stringP, Int16 stringSize) {
-  return DateTemplateToAscii(templateP, months, days, years, stringP, stringSize);
-}
-
-void DateGlueToDOWDMFormat(UInt8 month, UInt8 day, UInt16 year, DateFormatType dateFormat, Char *pString) {
-  DateToDOWDMFormat(month, day, year, dateFormat, pString);
-}
