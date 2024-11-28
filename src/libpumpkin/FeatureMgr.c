e75e1fbb9fad693077973f8b20f97d4d48f185d2
migueletto <migueletto@yahoo.com>
2024-11-28 19:09:02 -0300

    Implement some sysFtr numbers not present in the PalmSource SDK.

--- src/libpumpkin/FeatureMgr.c
+++ src/libpumpkin/FeatureMgr.c
@@ -11,6 +11,15 @@
 
 #define MAX_FEATURES 32
 
+#define sysFtrNumUIHardwareFlags        27           // Additional User Input Hardware (PalmOS 5.x)
+#define sysFtrNumDmAutoBackup           31           // Is Data Manager Auto Backup supported?
+#define sysFtrNumFiveWayNavVersion      32           // version of the 5-way nav if any
+
+#define sysFtrNumUIHardwareHas5Way      0x00000001   // device has a 5-way rocker
+#define sysFtrNumUIHardwareHasJog       0x00000002   // device has a jog wheel
+#define sysFtrNumUIHardwareHasJogBack   0x00000004   // device has a jog wheel with a 'back' button
+#define sysFtrNumUIHardwareHasKbd       0x00000008   // device has a dedicated keyboard
+
 typedef struct {
   UInt32 creator;
   UInt16 featureNum;
@@ -110,6 +119,19 @@ Err FtrGet(UInt32 creator, UInt16 featureNum, UInt32 *valueP) {
           }
           break;
         case sysFtrNumAccessorTrapPresent:
+          *valueP = 1;
+          err = errNone;
+          break;
+        case sysFtrNumUIHardwareFlags:
+          // XXX check if device has a keyboard (a Raspberry PI with just a touch screen should not set these flags)
+          *valueP = sysFtrNumUIHardwareHas5Way | sysFtrNumUIHardwareHasKbd;
+          err = errNone;
+          break;
+        case sysFtrNumDmAutoBackup:
+          *valueP = 0;
+          err = errNone;
+          break;
+        case sysFtrNumFiveWayNavVersion:
           *valueP = 1;
           err = errNone;
           break;
