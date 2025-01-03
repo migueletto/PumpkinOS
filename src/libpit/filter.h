cbe51a3a378e04ccb953e22a8cf73eca4c477ad9
migueletto <migueletto@yahoo.com>
2025-01-02 23:32:34 -0300

    Added close callback to filter.

--- src/libpit/filter.h
+++ src/libpit/filter.h
@@ -9,6 +9,7 @@ typedef struct conn_filter_t {
   int (*peek)(struct conn_filter_t *filter, uint32_t us);
   int (*read)(struct conn_filter_t *filter, uint8_t *b);
   int (*write)(struct conn_filter_t *filter, uint8_t *b, int n);
+  int (*close)(struct conn_filter_t *filter);
   void *data;
   struct conn_filter_t *next;
 } conn_filter_t;
