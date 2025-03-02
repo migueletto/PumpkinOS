ebb5157230cdccd7de2d9f49427066a081e0044a
migueletto <migueletto@yahoo.com>
2025-03-02 11:30:21 -0300

    Define Int64 and UInt64.

--- src/PalmOS/PalmTypes.h
+++ src/PalmOS/PalmTypes.h
@@ -68,10 +68,12 @@ typedef unsigned long   UInt32;
 typedef int8_t		Int8;
 typedef int16_t		Int16;	
 typedef int32_t		Int32;
+typedef int64_t		Int64;
 
 typedef uint8_t  	UInt8;
 typedef uint16_t  UInt16;
 typedef uint32_t  UInt32;
+typedef uint64_t  UInt64;
 #endif
 
 
