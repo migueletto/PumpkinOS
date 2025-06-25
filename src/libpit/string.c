89d6fce3fc4bd2e347d4f776a9e6f14fb2acadc1
migueletto <migueletto@yahoo.com>
2025-06-25 19:35:30 -0300

    Simpler implementation for sys_memset() in Kernel.

--- src/libpit/string.c
+++ src/libpit/string.c
@@ -206,37 +206,8 @@ void *sys_memmove(void *dest, const void *src, sys_size_t n) {
 
 void *sys_memset(void *dest, int c, sys_size_t n) {
 #if defined(KERNEL)
-  unsigned char *s = dest;
-  size_t k;
-
-  /* Fill head and tail with minimal branching. Each
-   * conditional ensures that all the subsequently used
-   * offsets are well-defined and in the dest region. */
-    
-  if (!n) return dest;
-  s[0] = c;
-  s[n-1] = c; 
-  if (n <= 2) return dest;
-  s[1] = c;
-  s[2] = c;
-  s[n-2] = c;
-  s[n-3] = c;
-  if (n <= 6) return dest;
-  s[3] = c;
-  s[n-4] = c;
-  if (n <= 8) return dest;
-  
-  /* Advance pointer to align it at a 4-byte boundary,
-   * and truncate n to a multiple of 4. The previous code
-   * already took care of any head/tail that get cut off
-   * by the alignment. */
-  
-  k = -(uintptr_t)s & 3;
-  s += k;
-  n -= k;
-  n &= -4;
-
-  for (; n; n--, s++) *s = c;
+  unsigned char *d = dest;
+  for (; n; n--) *d++ = c;
   return dest;
 #else
   return memset(dest, c, n);
