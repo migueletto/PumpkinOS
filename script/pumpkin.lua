5716a439a7e00466e831b66131662d5b17ba581f
migueletto <migueletto@yahoo.com>
2025-08-18 16:39:58 -0300

    Unified startup scripts.

--- script/pumpkin.lua
+++ script/pumpkin.lua
@@ -24,6 +24,15 @@ if not pit.getpointer("window_provider") and pit.getenv("DISPLAY") then
 end
 
 if not pit.getpointer("window_provider") then
+  fb = pit.loadlib("libfb")
+  if fb then
+    fb.setup(0, 1, 0)
+    fb.cursor(true)
+    hdepth = 16
+  end
+end
+
+if not pit.getpointer("window_provider") then
   print("window provider not found")
   pit.finish(0)
   return
