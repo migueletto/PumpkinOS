c5b82af76c15342c10147839ddfe1b21e49b9f3c
migueletto <migueletto@yahoo.com>
2025-09-06 08:48:51 -0300

    Set Emscripten window to 800x600

--- src/emscripten/vfs/pumpkin.lua
+++ src/emscripten/vfs/pumpkin.lua
@@ -14,13 +14,10 @@ pumpkin = pit.loadlib("libos")
 pumpkin.init()
 
 pumpkin.start {
-  mode        = 2,
-  dia         = true,
   fullrefresh = true,
   density     = 144,
   width       = 800,
   height      = 600,
   hdepth      = 16,
-  depth       = 16,
-  launcher    = "Launcher"
+  depth       = 16
 }
