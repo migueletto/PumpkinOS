d972baa05aabd92645ffb4a44ac26e7b57bd5912
migueletto <migueletto@yahoo.com>
2025-09-18 16:42:05 -0300

    Use git tag ID in pumpkin_get_build(), if available.

--- src/libpumpkin/build.c
+++ src/libpumpkin/build.c
@@ -4,12 +4,16 @@
 
 const char *pumpkin_get_build(void) {
 #if defined(GIT_COMMIT_ID)
+#if defined(GIT_TAG_NAME)
+  return GIT_TAG_NAME;
+#else
 #if defined(GIT_NOT_CLEAN)
   return GIT_COMMIT_ID "*";
 #else
   return GIT_COMMIT_ID;
-#endif
+#endif // GIT_NOT_CLEAN
+#endif // GIT_TAG_NAME
 #else
   return "<unknown>";
-#endif
+#endif // GIT_COMMIT_ID
 }
