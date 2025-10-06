4b758d7ef1afdfde50eec559a011bd6887b0783b
migueletto <migueletto@yahoo.com>
2025-10-06 16:06:00 -0300

    Added debug to ExgRegisterDatatype().

--- src/libpumpkin/ExgMgr.c
+++ src/libpumpkin/ExgMgr.c
@@ -3,121 +3,130 @@
 #include "debug.h"
 
 Err ExgInit(void) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgInit not implemented");
+  debug(DEBUG_ERROR, PUMPKINOS, "ExgInit not implemented");
   return sysErrParamErr;
 }
 
 Err ExgConnect(ExgSocketType *socketP) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgConnect not implemented");
+  debug(DEBUG_ERROR, PUMPKINOS, "ExgConnect not implemented");
   return sysErrParamErr;
 }
 
 Err ExgPut(ExgSocketType *socketP) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgPut not implemented");
+  debug(DEBUG_ERROR, PUMPKINOS, "ExgPut not implemented");
   return sysErrParamErr;
 }
 
 Err ExgGet(ExgSocketType *socketP) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgGet not implemented");
+  debug(DEBUG_ERROR, PUMPKINOS, "ExgGet not implemented");
   return sysErrParamErr;
 }
 
 Err ExgAccept(ExgSocketType *socketP) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgAccept not implemented");
+  debug(DEBUG_ERROR, PUMPKINOS, "ExgAccept not implemented");
   return sysErrParamErr;
 }
 
 Err ExgDisconnect(ExgSocketType *socketP, Err error) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgDisconnect not implemented");
+  debug(DEBUG_ERROR, PUMPKINOS, "ExgDisconnect not implemented");
   return sysErrParamErr;
 }
 
 UInt32 ExgSend(ExgSocketType *socketP, const void *bufP, UInt32 bufLen, Err *err) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgSend not implemented");
+  debug(DEBUG_ERROR, PUMPKINOS, "ExgSend not implemented");
   return 0;
 }
 
 UInt32 ExgReceive(ExgSocketType *socketP, void *bufP, UInt32 bufLen, Err *err) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgReceive not implemented");
+  debug(DEBUG_ERROR, PUMPKINOS, "ExgReceive not implemented");
   return 0;
 }
 
 Err ExgControl(ExgSocketType *socketP, UInt16 op, void *valueP, UInt16 *valueLenP) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgControl not implemented");
+  debug(DEBUG_ERROR, PUMPKINOS, "ExgControl not implemented");
   return sysErrParamErr;
 }
 
 Err ExgRegisterData(UInt32 creatorID, UInt16 id, const Char *dataTypesP) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgRegisterData not implemented");
+  debug(DEBUG_ERROR, PUMPKINOS, "ExgRegisterData not implemented");
   return sysErrParamErr;
 }
 
 Err ExgRegisterDatatype(UInt32 creatorID, UInt16 id, const Char *dataTypesP, const Char *descriptionsP, UInt16 flags) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgRegisterDatatype not implemented");
-  return sysErrParamErr;
+  char screator[8];
+
+  pumpkin_id2s(creatorID, screator);
+  if (dataTypesP) {
+    debug(DEBUG_ERROR, PUMPKINOS, "ExgRegisterDatatype register creator '%s' id 0x%04X dataTypes \"%s\" descriptions \"%s\" flags 0x%04X not implemented",
+      screator, id, dataTypesP, descriptionsP, flags);
+  } else {
+    debug(DEBUG_ERROR, PUMPKINOS, "ExgRegisterDatatype unregister creator '%s' id 0x%04X not implemented", screator, id);
+  }
+
+  return exgMemError;
 }
 
 Err ExgNotifyReceiveV35(ExgSocketType *socketP) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgNotifyReceiveV35 not implemented");
+  debug(DEBUG_ERROR, PUMPKINOS, "ExgNotifyReceiveV35 not implemented");
   return sysErrParamErr;
 }
 
 Err ExgNotifyReceive(ExgSocketType *socketP, UInt16 flags) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgNotifyReceive not implemented");
+  debug(DEBUG_ERROR, PUMPKINOS, "ExgNotifyReceive not implemented");
   return sysErrParamErr;
 }
 
 Err ExgNotifyGoto(ExgSocketType *socketP, UInt16 flags) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgNotifyGoto not implemented");
+  debug(DEBUG_ERROR, PUMPKINOS, "ExgNotifyGoto not implemented");
   return sysErrParamErr;
 }
 
 Err ExgDBRead(ExgDBReadProcPtr readProcP, ExgDBDeleteProcPtr deleteProcP, void *userDataP, LocalID *dbIDP, UInt16 cardNo, Boolean *needResetP, Boolean keepDates) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgDBRead not implemented");
+  debug(DEBUG_ERROR, PUMPKINOS, "ExgDBRead not implemented");
   return sysErrParamErr;
 }
 
 Err ExgDBWrite(ExgDBWriteProcPtr writeProcP, void *userDataP, const char *nameP, LocalID dbID, UInt16 cardNo) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgDBWrite not implemented");
+  debug(DEBUG_ERROR, PUMPKINOS, "ExgDBWrite not implemented");
   return sysErrParamErr;
 }
 
 Boolean ExgDoDialog(ExgSocketType *socketP, ExgDialogInfoType *infoP, Err *errP) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgDoDialog not implemented");
+  debug(DEBUG_ERROR, PUMPKINOS, "ExgDoDialog not implemented");
   return false;
 }
 
 Err ExgRequest(ExgSocketType *socketP) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgRequest not implemented");
+  debug(DEBUG_ERROR, PUMPKINOS, "ExgRequest not implemented");
   return sysErrParamErr;
 }
 
 Err ExgSetDefaultApplication(UInt32 creatorID, UInt16 id, const Char *dataTypeP) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgSetDefaultApplication not implemented");
+  debug(DEBUG_ERROR, PUMPKINOS, "ExgSetDefaultApplication not implemented");
   return sysErrParamErr;
 }
 
 Err ExgGetDefaultApplication(UInt32 *creatorIDP, UInt16 id, const Char *dataTypeP) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgGetDefaultApplication not implemented");
+  debug(DEBUG_ERROR, PUMPKINOS, "ExgGetDefaultApplication not implemented");
   return sysErrParamErr;
 }
 
 Err ExgGetTargetApplication(ExgSocketType *socketP, Boolean unwrap, UInt32 *creatorIDP, Char *descriptionP, UInt32 descriptionSize) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgGetTargetApplication not implemented");
+  debug(DEBUG_ERROR, PUMPKINOS, "ExgGetTargetApplication not implemented");
   return sysErrParamErr;
 }
 
 Err ExgGetRegisteredApplications(UInt32 **creatorIDsP, UInt32 *numAppsP, Char **namesP, Char **descriptionsP, UInt16 id, const Char *dataTypeP) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgGetRegisteredApplications not implemented");
+  debug(DEBUG_ERROR, PUMPKINOS, "ExgGetRegisteredApplications not implemented");
   return sysErrParamErr;
 }
 
 Err ExgGetRegisteredTypes(Char **dataTypesP, UInt32 *sizeP, UInt16 id) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgGetRegisteredTypes not implemented");
+  debug(DEBUG_ERROR, PUMPKINOS, "ExgGetRegisteredTypes not implemented");
   return sysErrParamErr;
 }
 
 Err ExgNotifyPreview(ExgPreviewInfoType *infoP) {
-  debug(DEBUG_ERROR, "PALMOS", "ExgNotifyPreview not implemented");
+  debug(DEBUG_ERROR, PUMPKINOS, "ExgNotifyPreview not implemented");
   return sysErrParamErr;
 }
