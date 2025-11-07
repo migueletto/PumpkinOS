7d0de8659b02ed9377bcfb97788662580677c271
migueletto <migueletto@yahoo.com>
2025-11-06 21:43:11 -0300

    Fixed script name for RPI.

--- pumpkin_rpi.sh
+++ pumpkin_rpi.sh
@@ -18,7 +18,7 @@ add_gpio 536  # 512 + 24
 add_gpio 537  # 512 + 25
 echo -e '\033[?17;0;0c' > /dev/tty1
 
-gdb --args ./pumpkin -d 1 -f pumpkin.log -s libscriptlua ./script/pumpkin_rpi.lua
+./pumpkin -d 1 -f pumpkin.log -s libscriptlua ./script/pumpkin_rpi.lua
 
 del_gpio 536
 del_gpio 537
