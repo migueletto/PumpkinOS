80ef765aa439aa66645c9d75618416230764b6a5
migueletto <migueletto@yahoo.com>
2026-09-01 18:15:43 -0300

    libpumpkin: some experiments with INetLib.

--- src/libpumpkin/INetMgr.c
+++ src/libpumpkin/INetMgr.c
@@ -1,30 +1,159 @@
 #include <PalmOS.h>
 #include <INetMgr.h>
 
+#include "bytes.h"
 #include "debug.h"
 
+typedef struct {
+  UInt16 config;
+  UInt32 flags;
+  DmOpenRef cacheRef;
+  UInt32 cacheSize;
+} INetLibData;
+
 Err INetLibOpen(UInt16 libRefnum, UInt16 config, UInt32 flags, DmOpenRef cacheRef, UInt32 cacheSize, MemHandle *inetHP) {
-  return inetErrConfigNotFound;
+  INetLibData *data;
+  MemHandle h;
+  Err err = inetErrTooManyClients;
+
+  if ((h = MemHandleNew(sizeof(INetLibData))) != NULL) {
+    if ((data = MemHandleLock(h)) != NULL) {
+      data->config = config;
+      data->flags = flags;
+      data->cacheRef = cacheRef;
+      data->cacheSize = cacheSize;
+      *inetHP = h;
+      MemHandleUnlock(h);
+      err = errNone;
+    }
+  }
+
+  return err;
 }
 
 Err INetLibClose(UInt16 libRefnum, MemHandle inetH) {
-  return inetErrConfigNotFound;
+  if (inetH) {
+    MemHandleFree(inetH);
+  }
+
+  return errNone;
 }
 
 Err INetLibSleep(UInt16 libRefnum) {
-  return inetErrConfigNotFound;
+  return errNone;
 }
 
 Err INetLibWake(UInt16 libRefnum) {
-  return inetErrConfigNotFound;
+  return errNone;
+}
+
+static Err INetLibSettingGetUInt32(void *buf, UInt16 *bufLenP, UInt32 value) {
+  Err err = inetErrSettingSizeInvalid;
+
+  if (buf && bufLenP && *bufLenP == sizeof(UInt32)) {
+    if (pumpkin_is_m68k()) {
+      put4b(value, (uint8_t *)buf, 0);
+    } else {
+      put4l(value, (uint8_t *)buf, 0);
+    }
+    err = errNone;
+  }
+
+  return err;
+}
+
+static Err INetLibSettingGetPtr(void *buf, UInt16 *bufLenP, void *value) {
+  UInt8 *ram;
+  Err err = inetErrSettingSizeInvalid;
+
+  if (buf && bufLenP) {
+    if (pumpkin_is_m68k()) {
+      if (*bufLenP == sizeof(UInt32)) {
+        ram = pumpkin_heap_base();
+        put4b((uint8_t *)value - ram, (uint8_t *)buf, 0);
+        err = errNone;
+      }
+    } else { 
+      if (*bufLenP == sizeof(void *)) {
+        sys_memcpy(buf, &value, sizeof(void *));
+      }
+    }
+  }
+    
+  return err;
 }
 
 Err INetLibSettingGet(UInt16 libRefnum, MemHandle inetH, UInt16 /*INetSettingEnum */ setting, void *bufP, UInt16 *bufLenP) {
-  return inetErrConfigNotFound;
+  INetLibData *data;
+  Err err = inetErrParamsInvalid;
+
+  if (inetH && bufP && bufLenP) {
+    if ((data = MemHandleLock(inetH)) != NULL) {
+      switch (setting) {
+        case inetSettingProxyType:           // (RW) UInt32, INetProxyEnum
+          err = INetLibSettingGetUInt32(bufP, bufLenP, inetProxyNone);
+          break;
+        case inetSettingProxyName:           // (RW) Char[], name of proxy
+          err = INetLibSettingGetUInt32(bufP, bufLenP, 0);
+          break;
+        case inetSettingProxyPort:           // (RW) UInt32,  TCP port # of proxy
+          err = INetLibSettingGetUInt32(bufP, bufLenP, 0);
+          break;
+        case inetSettingProxySocketType:     // (RW) UInt32, which type of socket to use netSocketTypeXXX
+          break;
+        case inetSettingCacheSize:           // (RW) UInt32, max size of cache
+          err = INetLibSettingGetUInt32(bufP, bufLenP, data->cacheSize);
+          break;
+        case inetSettingCacheRef:            // (R) DmOpenRef, ref of cache DB
+          err = INetLibSettingGetPtr(bufP, bufLenP, data->cacheRef);
+          break;
+        case inetSettingNetLibConfig:        // (RW) UInt32, Which NetLib config to use.
+          err = INetLibSettingGetUInt32(bufP, bufLenP, data->config);
+          break;
+        case inetSettingRadioID:             // (R)  UInt32[2], the 64-bit radio ID
+        case inetSettingBaseStationID:       // (R)  UInt32, the radio base station ID
+        case inetSettingMaxRspSize:          // (W) UInt32 (in bytes)
+        case inetSettingConvAlgorithm:       // (W) UInt32 (CTPConvEnum)
+        case inetSettingContentWidth:        // (W) UInt32 (in pixels)
+        case inetSettingContentVersion:      // (W) UInt32 Content version (encoder version)
+        case inetSettingNoPersonalInfo:      // (RW) UInt32 send no deviceID/zipcode
+        case inetSettingUserName:
+        case inetSettingGraphicsSel:         // (W) UInt8 (User Graphics selection)
+        case inetSettingTransportType:       // (RW) UInt32, INetTransportEnum
+        case inetSettingServerBits1:         // (RW) UInt32, bits sent by the server over ctp
+        case inetSettingSendRawLocationInfo: // (W) Boolean, make the handheld send its Raw Location information.
+        case inetSettingEnableCookies:       // (RW) Boolean
+        case inetSettingMaxCookieJarSize:    // (RW) UInt32, maximum cookie jar size in in kilobytes
+        case inetSettingLocRawInfo:          // (R) void* Allocated memory buffer - must be free by caller
+        case inetSettingProxyNameDefault:    // Default Name for this config
+        case inetSettingProxyPortDefault:    // Default Port for this config
+        case inetSettingProxyNameEditable:   // Is the proxy name editable?
+        case inetSettingProxyPortEditable:   // Is the proxy port editable?
+        case inetSettingPalmUserID:          // // The palm.net user id
+          break;
+      }
+      MemHandleUnlock(inetH);
+    }
+  }
+
+  return err;
 }
 
 Err INetLibSettingSet(UInt16 libRefnum, MemHandle inetH, UInt16 /*INetSettingEnum*/ setting, void *bufP, UInt16 bufLen) {
-  return inetErrConfigNotFound;
+  INetLibData *data;
+  Err err = inetErrParamsInvalid;
+
+  if (inetH && bufP) {
+    if ((data = MemHandleLock(inetH)) != NULL) {
+      switch (setting) {
+        default:
+          break;
+      }
+      MemHandleUnlock(inetH);
+    }
+  }
+
+  return err;
 }
 
 void INetLibGetEvent(UInt16 libRefnum, MemHandle inetH, INetEventType *eventP, Int32 timeout) {
--- src/libpumpkin/emulation/inetlibtrap.c
+++ src/libpumpkin/emulation/inetlibtrap.c
@@ -28,16 +28,17 @@ void palmos_inetlibtrap(uint16_t trap) {
       // Err INetLibOpen(UInt16 libRefnum, UInt16 config, UInt32 flags, DmOpenRef cacheRef, UInt32 cacheSize, MemHandle *inetHP)
       uint16_t refNum = ARG16;
       uint16_t config = ARG16;
-      uint16_t flags = ARG16;
+      uint16_t flags = ARG16; // unused by PalmOS
       uint32_t cacheRefP = ARG32;
       uint32_t cacheSize = ARG32;
-      uint32_t inetHPP = ARG32;
-      emupalmos_trap_in(cacheRefP, trap, 3);
-      emupalmos_trap_in(inetHPP, trap, 5);
+      uint32_t inetHP = ARG32;
+      DmOpenRef cacheRef = (DmOpenRef)emupalmos_trap_in(cacheRefP, trap, 3);
+      emupalmos_trap_in(inetHP, trap, 5);
       MemHandle inetH;
-      err = INetLibOpen(refNum, config, flags, NULL, cacheSize, &inetH);
+      err = INetLibOpen(refNum, config, flags, cacheRef, cacheSize, &inetH);
+      if (inetHP) m68k_write_memory_32(inetHP, emupalmos_trap_out(inetH));
       debug(DEBUG_TRACE, "EmuPalmOS", "INetLibOpen(refNum=%u, config=%u, flags=0x%04X, cacheRef=0x%08X, cacheSize=%u, inetHP=0x%08X): %d",
-        refNum, config, flags, cacheRefP, cacheSize, inetHPP, err);
+        refNum, config, flags, cacheRefP, cacheSize, inetHP, err);
       m68k_set_reg(M68K_REG_D0, err);
       }
       break;
@@ -62,6 +63,24 @@ void palmos_inetlibtrap(uint16_t trap) {
       uint16_t refNum = ARG16;
       err = NetLibWake(refNum);
       debug(DEBUG_TRACE, "EmuPalmOS", "INetLibWake(refNum=%d): %d", refNum, err);
+      m68k_set_reg(M68K_REG_D0, err);
+      }
+      break;
+    case inetLibTrapSettingGet: {
+      // Err INetLibSettingGet(UInt16 libRefnum, MemHandle inetH, UInt16 /*INetSettingEnum */ setting, void *bufP, UInt16 *bufLenP)
+      uint16_t refNum = ARG16;
+      uint32_t inetHP = ARG32;
+      uint16_t setting = ARG16;
+      uint32_t bufP = ARG32;
+      uint32_t bufLenP = ARG32;
+      MemHandle inetH = emupalmos_trap_in(inetHP, trap, 1);
+      void *buf = emupalmos_trap_in(inetHP, trap, 3);
+      emupalmos_trap_in(bufLenP, trap, 4);
+      UInt16 bufLen;
+      err = INetLibSettingGet(refNum, inetH, setting, buf, &bufLen);
+      if (bufLenP) m68k_write_memory_16(bufLenP, bufLen);
+      debug(DEBUG_TRACE, "EmuPalmOS", "INetLibSettingGet(refNum=%d, inetH=0x%08X, setting=%u, bufP=0x%08X, bufLenP=0x%08X): %d",
+        refNum, inetHP, setting, bufP, bufLenP, err);
       m68k_set_reg(M68K_REG_D0, err);
       }
       break;
