2135f367db4095209c8ba6803c1f2ef56c2a3def
migueletto <migueletto@yahoo.com>
2026-09-17 12:06:09 -0300

    libpumpkin: removing test code for dbIDs.

--- src/libpumpkin/dbid.h
+++
@@ -1,8 +0,0 @@
-typedef struct dbid_t dbid_t;
-
-dbid_t *dbid_init(void);
-void dbid_finish(dbid_t *d);
-uint32_t dbid_add(dbid_t *d, char *name, uint32_t id);
-uint32_t dbid_get(dbid_t *d, char *name);
-int dbid_valid(uint32_t id);
-void dbid_remove(dbid_t *d, char *name);
