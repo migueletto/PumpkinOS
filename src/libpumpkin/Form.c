12e9122e2a9fd74ad8a616c7a792339f590d9196
migueletto <migueletto@yahoo.com>
2026-09-17 12:03:15 -0300

    libpumpkin: documented the incompatibilities when setting the colorTable for a new form.

--- src/libpumpkin/Form.c
+++ src/libpumpkin/Form.c
@@ -201,8 +201,11 @@ static Err FrmInitFormInternal(FormType *formP) {
 
   WinScreenGetAttribute(winScreenDensity, &density);
   WinScreenMode(winScreenModeGet, NULL, NULL, &depth, NULL);
-  //colorTable = WinGetColorTable(depth);
-  colorTable = BmpGetColortable(WinGetBitmap(WinGetDisplayWindow()));
+
+  // XXX how the colorTable should be initialized?
+  //colorTable = WinGetColorTable(depth); // this causes palette problmes in FreeGrid
+  colorTable = BmpGetColortable(WinGetBitmap(WinGetDisplayWindow())); // this causes palette problems in eReader and PalmVNC
+
   //formP->window.bitmapP = BmpCreate3(w, h, 0, density, depth, false, 0, colorTable, &err);
   //formP->window.density = density;
   if ((bmp = BmpCreate3(w, h, 0, density, depth, false, 0, colorTable, &err)) == NULL) {
