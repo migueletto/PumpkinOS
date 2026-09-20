e6e5b12bedbc02794d44f451dc77508bcb7d8b5e
migueletto <migueletto@yahoo.com>
2026-09-20 00:08:23 -0300

    libpumpkin: temporary change: in emupalmos_check_address, panic only for write access.

--- src/libpumpkin/emulation/emupalmos.c
+++ src/libpumpkin/emulation/emupalmos.c
@@ -268,7 +268,7 @@ int emupalmos_check_address(uint32_t address, uint32_t size, int read) {
 #ifdef HEAP_DEBUG
   if (valid) {
     valid = pumpkin_heap_debug_access(address, size, read);
-    if (!valid) return valid;
+    if (!valid && read) return valid;
   }
 #endif
 
